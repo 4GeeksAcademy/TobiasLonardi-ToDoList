3d251a9ed0397cc95617c8a1b029c9fa2f38d999
TobiLonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-04-29 15:06:11 +0000

    Primer push de toDoList

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,61 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const[task, setTask] = useState("")
+
+	const [toDos,setToDos] = useState([]);
+
+	const addToList = (event) => {
+		if(event.key == "Enter"){
+			setToDos(
+				...toDos,
+				task
+			)
+		}
+	}
+
+	const changeController=(event)=>{
+		setTask(
+			event.target.value
+		)
+		console.log(task)
+
+	}
+
+	return (
+		<div className="container m-5 ">
+			<div className="row d-flex justify-content-center align-items-center">
+				<h1 className="text-center">
+					todos
+				</h1>
+				<div className="col-12 border">
+					<form action="">
+						<input type="text"
+							placeholder="What needs to be done?"
+							name="task"
+							onChange={changeController}
+							onKeyDown={addToList}
+						/>
+					</form>
+				</div>
+				<ul>
+					{
+						toDos.map((value,index) => {
+							return(
+								<li key={index}>{value} </li>
+							)
+						})
+					}
+				</ul>
+			</div>
 		</div>
+
 	);
 };
 
