0f83af4f873f137243151e95fb99379b24ba7a30
TobiLonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-05-01 15:10:35 +0000

    proyecto finalizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,62 +7,70 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 
-	const[task, setTask] = useState("")
+	const [task, setTask] = useState("")
 
-	const [toDos,setToDos] = useState([]);
+	const [toDos, setToDos] = useState([]);
 
 	const addToList = (event) => {
-		if(event.key == "Enter" && task!=""){
+		if (event.key == "Enter" && task.trim() != "") {
 			setToDos(
 				[...toDos,
-				task]
+					task]
 			)
 			setTask("")
 		}
 	}
 
-	const changeController=(event)=>{
+	const changeController = (event) => {
 		setTask(
 			event.target.value
 		)
 
 	}
 
-	const eliminateElement= (position) =>{
-		const result = toDos.filter((item,index) => index!=position )
+	const eliminateElement = (position) => {
+		const result = toDos.filter((item, index) => index != position)
 		setToDos(result)
 	}
 
 	return (
-		<div className="container m-5 ">
-			<div className="row d-flex justify-content-center align-items-center text-center">
-				<h1 className="text-center">
+		<div className="container py-5  ">
+			<div className="row justify-content-center">
+				<h1 className="text-center text-body-secondary">
 					todos
 				</h1>
-				<div className="col-12">
-					<form action="" className="form-control form-control-lg" onSubmit={(event) => event.preventDefault()}>
+				<div className="col-12 col-md-7 shadow bg-body-tertiary p-0 m-0">
+					<form action="" className="bg-body-tertiary" onSubmit={(event) => event.preventDefault()}>
 						<input type="text"
 							placeholder="What needs to be done?"
+							className="form-control form-control-lg  bg-body-tertiary text-body-secondary"
 							name="task"
 							onChange={changeController}
 							value={task}
 							onKeyDown={addToList}
 						/>
 					</form>
+					<ul className="">
+						{
+							toDos.map((value, index) => {
+								return (
+									<li key={index} className="border rounded text-body-secondary">{value}
+									<span>
+										<i className="fa-solid fa-xmark" onClick={() => eliminateElement(index)}></i>
+									</span>
+									</li>
+								)
+							})
+						}
+					</ul>
+					<div className="d-flex justify-content-start align-items-center border rounded p-1">
+					<h6 className="text-center text-body-secondary">item left {toDos.length}</h6>
+
+					</div>
+
 				</div>
-				<ul>
-					{
-						toDos.map((value,index) => {
-							return(
-								<li key={index} className="border rounded">{value}
-									<i className="fa-solid fa-xmark" onClick={() => eliminateElement(index)}></i>
-								</li>
-							)
-						})
-					}
-				</ul>
+
 			</div>
-			<h6 className="text-center border-bottom">item left {toDos.length}</h6>
 
 		</div>
 
